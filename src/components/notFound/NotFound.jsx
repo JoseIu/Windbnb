58539abfd71a3b7a4c53c03e1310a1aa413113e2
Joselu <joseluissgz97@gmail.com>
2023-07-13 12:54:02 +0200

    adding styles for NotFound component

--- src/components/notFound/NotFound.jsx
+++ src/components/notFound/NotFound.jsx
@@ -1,7 +1,8 @@
+import './notFound.scss';
 const NotFound = () => (
-	<div className='wrapper'>
-		<p>No results</p>
-		<img src='../../../public/notFound.gif' alt='' />
+	<div className='notFound wrapper'>
+		<p className='notFound__p'>No results</p>
+		<img className='notFound__img' src='../../../public/notFound.gif' alt='' />
 	</div>
 );
 
